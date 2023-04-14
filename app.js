34093f218fa4e1a80d09d6c77a1ab924b52b8488
Aquib Alam <aquibalam399@gmail.com>
2023-04-14 19:14:40 +0530

    thank page

--- app.js
+++ app.js
@@ -33,8 +33,9 @@ app.post("/send", async (req, res) => {
         if (await db.findOne({ mail })) {
             throw { status: 302 };
         }
+        // otp = Math.floor(Math.random() * 89999 + 10000);
         otp = await require("./scripts/mail").sendMail(mail);
-        console.log(otp);
+        // console.log(otp);
         sent = true;
         dt = Date.now();
         res.send({ status: 200 });
--- scripts/script.js
+++ scripts/script.js
@@ -45,7 +45,7 @@ async function checkOtp() {
         alert("Error while Verifying");
         return;
     }
-    alert("OTP Verified");
+    document.write(thank());
 }
 
 (() => {
@@ -54,6 +54,51 @@ async function checkOtp() {
     let str = "",
         cnt = 0;
     const x = setInterval(() => {
-        msg.length > cnt ? ((str += msg[cnt++]), (pg.innerHTML = str + "_")) : (clearInterval(x), (pg.innerHTML = str));
+        msg.length > cnt ? ((str += msg[cnt++]), (pg.innerHTML = str + String.fromCharCode(Math.floor(Math.random() * 89 + 33)))) : (clearInterval(x), (pg.innerHTML = str));
     }, 80);
 })();
+function thank() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Thank You</title>
+        </head>
+        <body style="background-color: black; display: flex; justify-content: center; align-items: center; min-height: 90vh">
+            <div class="container" style="display: flex; justify-content: center; text-align: center">
+                <div class="item">
+                    <img src="./tick.png" alt="" width="70px" />
+                    <p id="tnk" style="font-family: Montserrat; font-size: large; color: white; text-shadow: 0px 0px 50px white; -webkit-box-reflect: below 0px linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.1))"></p>
+                </div>
+            </div>
+            <footer style="position: fixed; bottom: 10%">
+                <img src="../pages/insta.png" alt="Instagram" style="filter: invert(100%); margin: 3px; width: 35px" />
+                <img src="../pages/linked.png" alt="Linkedin" style="filter: invert(100%); margin: 3px; width: 35px" />
+            </footer>
+        </body>
+    </html>
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap");
+        ::selection {
+            background-color: white;
+            color: black;
+        }
+    </style>
+    <script>
+        (() => {
+            const pg = document.getElementById("tnk");
+            const msg = "Thank you for being part of us";
+            let str = "",
+                cnt = 0;
+            function ran() {
+                return String.fromCharCode(Math.floor(Math.random() * 89 + 33));
+            }
+            const x = setInterval(() => {
+                msg.length > cnt ? ((str += msg[cnt++]), (pg.innerHTML = str + ran() + ran() + ran())) : (clearInterval(x), (pg.innerHTML = str));
+            }, 60);
+        })();
+    </script>
+    `;
+}
