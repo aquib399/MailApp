eac43bf27b75adae4576b9bef7134fdc825774f7
Aquib Alam <aquibalam399@gmail.com>
2023-04-21 23:20:43 +0530

    comment

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ let data = {};
 function checkTime(mail) {
     let temp = Date.now();
     if (temp - 30000 > data[mail].time) {
-        //Can send
+        // Can send
         return false;
     }
-    //Can not send
+    // Can not send
     let remaining = Math.round((30000 - (temp - data[mail].time)) / 1000);
     return remaining;
 }
@@ -27,56 +27,56 @@ app.get("/", (req, res) => {
 app.post("/send", async (req, res) => {
     let mail = req.body.mail;
     try {
-        //check if the otp was sent or not
+        // check if the otp was sent or not
         if (data[mail]) {
-            //check if 30 seconds are passed or not
+            // check if 30 seconds are passed or not
             const time = checkTime(mail);
             if (time) {
-                //Cant send because its already sent
+                // Cant send because its already sent
                 // console.log("Please wait", time, "seconds");
-                //Send response as already sent and the time remaining
+                // Send response as already sent and the time remaining
                 throw { status: 412, time };
                 return;
             }
         }
-        //check if the mail exist in the database or not
+        // check if the mail exist in the database or not
         if (await db.findOne({ mail })) {
             // console.log("Already exist");
             throw { status: 302 };
             return;
         }
-        //create a instance(mail's details) in the data object
+        // create a instance(mail's details) in the data object
         data[mail] = {};
         // data[mail].otp = Math.floor(Math.random() * 89999 + 10000);
         data[mail].otp = await require("./scripts/mail").sendMail(mail);
         data[mail].time = Date.now();
         // console.log("Mail sent at->", mail, data[mail].otp);
-        //Send response as 'Mail sent'
+        // Send response as 'Mail sent'
         throw { status: 200 };
     } catch (e) {
         res.send({ status: e.status || 500, time: e.time });
     }
 });
 app.post("/insert", async (req, res) => {
-    //Gather the body data
+    // Gather the body data
     let mail = req.body.mail;
     let otp = req.body.otp;
     try {
-        //check if the mail exist in the database or not
+        // check if the mail exist in the database or not
         if (await db.findOne({ mail })) {
             // console.log("Already exist");
             throw { status: 302 };
         }
-        //check the corrosponding body.data from data object
+        // check the corrosponding body.data from data object
         if (data[mail].otp == otp) {
             //OTP verified
             await db.insertOne({ mail });
             delete data[mail];
             // console.log("Verified & inserted");
-            //send response as successfully inserted
+            // send response as successfully inserted
             throw { status: 200 };
         }
-        //Not verfied
+        // Not verfied
         // console.log("Wrong otp");
         // send response as not verfied
         throw { status: 401 };
@@ -84,19 +84,15 @@ app.post("/insert", async (req, res) => {
         res.send({ status: e.status });
     }
 });
-app.listen(3000, () => {
-    console.clear();
-    // console.log("http://localhost:3000");
-});
+app.listen(3000);
 
-//For deleting the mail's details if not verfied in 30 seconds in every 5 minutes
+// For deleting the mail's details if not verfied in 30 seconds in every 5 minutes
 setInterval(() => {
-    let temp = Date.now();
+    let temp = Date.now() - 30000;
     Object.entries(data).forEach(([key, val]) => {
         // console.log({ key, val });
-        if (temp - 30000 > val.time) {
+        if (temp > val.time) {
             delete data[key];
         }
     });
-    // console.log();
 }, 300000);
